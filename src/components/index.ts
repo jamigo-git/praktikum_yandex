df9aba2efad1a8a9a2a750fdc14ef829daac43fd
Igor Yamaleev <jamigo@ya.ru>
2024-04-08 21:28:21 +0400

    Еще правка страниц

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
-export { Button } from "./button"
-export { Input } from "./input"
-export { ErrorPage } from "./error"
-export { FormString } from "./form_string"
-export { Avatar } from "./avatar"
-export { ButtonNav } from "./button_nav"
-export { ButtonMenu } from "./button_menu"
+export { Button } from "./button";
+export { Input } from "./input";
+export { ErrorPage } from "./error";
+export { FormString } from "./form_string";
+export { Avatar } from "./avatar";
+export { ButtonNav } from "./button_nav";
+export { ButtonMenu } from "./button_menu";
+
