5f0199d0e2d2a46574d7650b5c84021bf8539f21
Igor Yamaleev <jamigo@ya.ru>
2024-04-19 08:32:15 +0400

    chat_item, chat_list drafts done

---
+++ src/components/chat_list/index.ts
@@ -0,0 +1,17 @@
+import "./chat_list.css";
+import Handlebars from 'handlebars';
+import ava from "../../assets/z.jpg"
+import fly from "../../assets/fly.jpeg"
+import robot from "../../assets/images.jpeg"
+
+export { default as ChatList } from "./chat_list.hbs?raw";
+
+
+
+Handlebars.registerHelper('chat_items', () => {
+    return [
+        {label: 'Петрович', avatar: ava, text: 'random text 123...', datetime: '14:43', counter: 2},
+        {label: 'Василий Иванович', avatar: fly, active: true, text: 'random text 123...', datetime: '10:15', counter: 44},
+        {label: 'Ларгукус', avatar: robot, text: 'random text 123...', datetime: '15 января'},
+    ]
+})
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,6 @@ export { FormString } from "./form_string";
 export { Avatar } from "./avatar";
 export { ButtonNav } from "./button_nav";
 export { ButtonMenu } from "./button_menu";
+export { ChatList } from "./chat_list";
+export { ChatItem } from "./chat_item";
 
