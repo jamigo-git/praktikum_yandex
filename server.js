883ffddbf54d1f9ad6bd856bed95365ce343613c
Igor Yamaleev <jamigo@ya.ru>
2024-05-20 23:18:31 +0400

    server fix 3

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import path from 'path';
 
 const PORT = 3000;
 const app = express();
-const entryPoint = path.resolve(__dirname, 'dist', 'index.html');
-
-app.use("*", (req, res) => {
-    res.sendFile(`${__dirname}/dist/index.html`);
-});
+app.use(express.static('./dist'));
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
