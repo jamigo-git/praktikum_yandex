38865f2939a5628f34fe8aa00ba2bcc29c33ebd5
Igor Yamaleev <jamigo@ya.ru>
2024-05-20 23:24:07 +0400

    server fix 4

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+Server
+
 import express from 'express';
+import path from 'path';
+import history from './http-rewrite/history-plugin';
 
 const PORT = 3000;
 const app = express();
-app.use(express.static(__dirname + './dist'));
+const entryPoint = path.resolve(__dirname, 'dist', 'index.html');
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(history);
+
+app.use("*", (req, res) => {
+    res.sendFile(`${__dirname}/dist/index.html`);
+});
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
+
