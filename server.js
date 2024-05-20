5da3a3e0293f9c08bccf57db7310d0ed25017a7f
Igor Yamaleev <jamigo@ya.ru>
2024-05-20 23:29:17 +0400

    server fix again

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 Server
 
 import express from 'express';
-import path from 'path';
-import history from './http-rewrite/history-plugin';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
 
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
-const entryPoint = path.resolve(__dirname, 'dist', 'index.html');
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
-app.use(history);
+// const entryPoint = path.resolve(__dirname, 'dist', 'index.html');
+app.use(express.static(resolve(__dirname, 'dist')));
+// app.use(history);
 
 app.use("*", (req, res) => {
     res.sendFile(`${__dirname}/dist/index.html`);
