16fbeb8a8bf05a8ff0a064da16f6b6403bb6f16c
Igor Yamaleev <jamigo@ya.ru>
2024-05-20 23:21:38 +0400

    sesrver fix 5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const PORT = 3000;
 const app = express();
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + './dist'));
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
